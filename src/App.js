b2c12c59cce53bc5d044f59ebc712dc9a8cd1763
thailonlucas <thailon.lucas28@gmail.com>
2022-03-17 01:56:17 -0300

    Add holographic stripe

--- src/App.js
+++ src/App.js
@@ -9,18 +9,21 @@ import { useDeviceOrientation } from './logic/device-orientation-hook';
 
 function App() {
   const logoSvg = useRef(null)
+  const stripe = useRef(null)
   const [orientation] = useDeviceOrientation()
 
   useEffect(() => {
-    if(!logoSvg.current) return
+    if(!logoSvg.current || !stripe.current) return
     let x = interpolate(orientation.gamma, -70, 70, 0, 100)
     let y = interpolate(orientation.beta, -160, 160, 0, 100)
     logoSvg.current.style.backgroundPosition = `${x}% ${y}%`
+    stripe.current.style.backgroundPosition = `${x}% ${y}%`
   }, [logoSvg, orientation])
   
   return (
     <div className='home-screen'>
       <img className='holo-bg' ref={logoSvg}/>
+      <img className='holo-bg2'  ref={stripe}/>
       <NameClipPath/>
     </div>
   )
